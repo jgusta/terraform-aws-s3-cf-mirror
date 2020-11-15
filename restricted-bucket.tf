0b476a92bb6d1bd7750862abbd1b3d252eec04c6
jgusta <jgustafx@gmail.com>
2020-11-14 19:57:18 -0800

    versions

--- restricted-bucket.tf
+++ restricted-bucket.tf
@@ -1,69 +1,76 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "3.15.0"
+    required_providers {
+        cloudflare =  {
+            source = "cloudflare/cloudflare"
+            version = "~> 2.0"
+        }
+        aws =  {
+            source = "hashicorp/aws"
+            version = "3.15.0"
+        }
     }
-  }
 }
 
 provider "aws" {
-  # Configuration options
-  region = "us-west-2"
+    # Configuration options
+    region = "us-west-2"
+}
+
+provider "cloudflare" {
+    version = "~> 2.0"
 }
 
 data "cloudflare_ip_ranges" "cloudflare" {}
 
 resource "aws_s3_bucket" "static-bucket" {
-  bucket = var.BUCKET_NAME
-  acl    = "private"
+    bucket = var.BUCKET_NAME
+    acl = "private"
+    website {
+        index_document = "index.html"
+        error_document = "index.html"
+    }
 
-  website {
-    index_document = "index.html"
-    error_document = "index.html"
-  }
-
-  tags = {
-    Name = var.BUCKET_NAME
-  }
+    tags =  {
+        Name = var.BUCKET_NAME
+    }
 }
 
 
 #
 # Restricting only to IPv4 addresses. For IPv6 use data.cloudflare_ip_ranges.cloudflare.ipv6_cidr_blocks
 #
-data "aws_iam_policy_document" "restrict-to-cloudflare-ips" {
-  statement {
-    sid = "RestrictToCloudflareIPs"
+data "aws_iam_policy_document" "restrict_to_cloudflare_ips" {
+    statement {
+        sid = "RestrictToCloudflareIPs"
 
-    actions = [
-      "s3:*"
-    ]
+        actions = [
+            "s3:*"
+        ]
 
-    effect = "Deny"
+        effect = "Deny"
 
-    resources = [
-      "arn:aws:s3:::${var.BUCKET_NAME}",
-      "arn:aws:s3:::${var.BUCKET_NAME}/*"
-    ]
+        resources = [
+            "arn:aws:s3:::${var.BUCKET_NAME}", 
+            "arn:aws:s3:::${var.BUCKET_NAME}/*"
+        ]
 
-    not_principals {
-      type = "*"
-      identifiers = [
-        "arn:aws:iam::${var.ACCOUNT_ID}:root",
-        "arn:aws:iam::${var.ACCOUNT_ID}:user/&{aws:username}"
-      ]
+        not_principals {
+            type = "*"
+            identifiers = [
+                "arn:aws:iam::${var.ACCOUNT_ID}:root",
+                "arn:aws:iam::${var.ACCOUNT_ID}:user/&{aws:username}"
+            ]
+        }
+
+        condition {
+            test = "NotIpAddress"
+            variable = "aws:SourceIp"
+            values = data.cloudflare_ip_ranges.cloudflare.ipv4_cidr_blocks
+        }
     }
-
-    condition {
-      test     = "NotIpAddress"
-      variable = "aws:SourceIp"
-      values   = data.cloudflare_ip_ranges.cloudflare.ipv4_cidr_blocks
-    }
-  }
 }
 
 resource "aws_s3_bucket_policy" "static-bucket" {
-  bucket = aws_s3_bucket[var.BUCKET_NAME].id
-  policy = data.aws_iam_policy_document.restrict-to-cloudflare-ips.json
+    bucket = aws_s3_bucket[var.BUCKET_NAME].id
+    policy = data.aws_iam_policy_document.restrict_to_cloudflare_ips.json
 }
